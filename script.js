253884f734dcf956380e1fe6db2f51a30c1393be
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-09-19 16:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let timerDisplay=document.getElementById("timer");
+ let currentDate= new Date();
+ setIntervel{
+	 
+	 function(){
+       currentDate=new Date();
+		 timerDisplay.innerHTML=currentDate.toLocalString();
+	  
+	 
+	 },1000
+
+ };
